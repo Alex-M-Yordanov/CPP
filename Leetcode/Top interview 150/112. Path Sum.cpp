1fe95c49968c5226f2c071735614f8bab341250a
Alex <59801171+Alex-M-Yordanov@users.noreply.github.com>
2024-07-30 20:12:40 +0300

    Create 112. Path Sum.cpp

---
+++ Leetcode/Top interview 150/112. Path Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+      
+        if(!root)
+        {
+            return false;
+        } 
+      
+        if(!root->left && !root->right && targetSum==root->val )
+        {
+            return true;
+        }
+        
+        bool left = hasPathSum(root->left, targetSum - root->val);
+        bool right = hasPathSum(root->right, targetSum - root->val);
+
+        return left || right;
+    }
+};
